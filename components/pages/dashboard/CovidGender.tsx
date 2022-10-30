ffeae4b13161e3494db969424de18f001ad4d91c
woojeong kong <coolstevevc@gmail.com>
2022-10-30 16:46:51 +0900

    feat(component): add CovidGender component
    The number of confirmed cases of covid by gender is shown using a doughnut chart

---
+++ components/pages/dashboard/CovidGender.tsx
@@ -0,0 +1,140 @@
+import { useState, useMemo, ChangeEvent } from "react";
+import styled from "@emotion/styled";
+import moment from "moment";
+import { ChartData } from "chart.js";
+
+import ChartSection from "../../chart/ChartSection";
+import DoughnutChart from "../../chart/DoughnutChart";
+
+interface CovidGenAgeInfo {
+  confCase: string;
+  stateDt: string;
+  gubun: string;
+}
+
+interface Props {
+  data: CovidGenAgeInfo[];
+}
+
+interface DoughnutDailyData {
+  [key: string]: ChartData<"doughnut">;
+}
+
+interface CovidDailyGenderRatioInfo {
+  [key: string]: number[];
+}
+
+const options = {
+  plugins: {
+    legend: {
+      display: true,
+      position: "bottom" as const,
+    },
+    title: {
+      display: false,
+    },
+  },
+  maintainAspectRatio: false,
+};
+
+function CovidGender({ data }: Props) {
+  const [selectedDate, setSelectedDate] = useState<string>("");
+
+  const { doughnutLabels, doughnutDailyData } = useMemo(() => {
+    const covidDailyGenderRatioInfo: CovidDailyGenderRatioInfo = {};
+    const doughnutLabels = new Set<string>();
+
+    data.forEach((item) => {
+      if (item.gubun === "남성" || item.gubun === "여성") {
+        doughnutLabels.add(item.stateDt);
+
+        if (!covidDailyGenderRatioInfo[item.stateDt]) {
+          covidDailyGenderRatioInfo[item.stateDt] = [0, 0];
+        }
+
+        if (item.gubun === "남성") {
+          covidDailyGenderRatioInfo[item.stateDt][0]++;
+        }
+
+        if (item.gubun === "여성") {
+          covidDailyGenderRatioInfo[item.stateDt][1]++;
+        }
+      }
+    });
+
+    const doughnutDailyData: DoughnutDailyData = {};
+
+    [...doughnutLabels].forEach((item) => {
+      doughnutDailyData[item] = {
+        labels: ["남성", "여성"],
+        datasets: [
+          {
+            label: "gender ratio",
+            data: covidDailyGenderRatioInfo[item],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+            ],
+            borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)"],
+            borderWidth: 1,
+          },
+        ],
+      };
+    });
+
+    return { doughnutLabels, doughnutDailyData };
+  }, []);
+
+  function handleDateSelect(event: ChangeEvent<HTMLSelectElement>) {
+    setSelectedDate(event.target.value);
+  }
+
+  return (
+    <ChartSection title="코로나 일자별 확진자 수">
+      <Wrapper>
+        <div className="select-section">
+          <select onChange={handleDateSelect} value={selectedDate}>
+            {[...doughnutLabels].reverse().map((item) => {
+              return (
+                <option key={item} value={item}>
+                  {moment(item).format("MM/DD")}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div className="chart-section">
+          <DoughnutChart
+            options={options}
+            data={
+              selectedDate === ""
+                ? doughnutDailyData[[...doughnutLabels].reverse()[0]]
+                : doughnutDailyData[selectedDate]
+            }
+          />
+        </div>
+      </Wrapper>
+    </ChartSection>
+  );
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  .select-section {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+  }
+
+  .chart-section {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export default CovidGender;
