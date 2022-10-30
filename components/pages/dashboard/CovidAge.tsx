839c2ff0a664f355913a84cbe94381850aaf9c9d
woojeong kong <coolstevevc@gmail.com>
2022-10-30 15:11:04 +0900

    feat(component): add CovidAge component
    The number of confirmed cases of covid by age is shown using a bar chart

---
+++ components/pages/dashboard/CovidAge.tsx
@@ -0,0 +1,112 @@
+import moment from "moment";
+
+import ChartSection from "../../chart/ChartSection";
+import StackedBarChart from "../../chart/StackedBarChart";
+
+interface CovidGenAgeInfo {
+  confCase: string;
+  stateDt: string;
+  gubun: string;
+}
+
+interface CovidAgeInfo {
+  [key: string]: {
+    [key: string]: string;
+  };
+}
+
+interface Props {
+  data: CovidGenAgeInfo[];
+}
+
+const options = {
+  indexAxis: "y" as const,
+  plugins: {
+    legend: {
+      display: true,
+      position: "bottom" as const,
+    },
+    title: {
+      display: false,
+    },
+  },
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      stacked: true,
+    },
+  },
+  maintainAspectRatio: false,
+};
+
+const ageGubunList = [
+  "0-9",
+  "10-19",
+  "20-29",
+  "30-39",
+  "40-49",
+  "50-59",
+  "60-69",
+  "70-79",
+  "80 이상",
+];
+
+const colorInfo = {
+  "0-9": "rgb(255, 99, 132)",
+  "10-19": "rgb(75, 192, 192)",
+  "20-29": "rgb(1, 99, 132)",
+  "30-39": "rgb(53, 162, 235)",
+  "40-49": "rgb(255, 99, 1)",
+  "50-59": "rgb(255, 1, 132)",
+  "60-69": "rgb(20, 50, 100)",
+  "70-79": "rgb(255, 1, 1)",
+  "80 이상": "rgb(100, 100, 100)",
+};
+
+function CovidAge({ data }: Props) {
+  const covidAgeInfo: CovidAgeInfo = {};
+  const barLabels = new Set<string>();
+
+  data.forEach((item) => {
+    if (item.gubun === "남성" || item.gubun === "여성") {
+      return;
+    }
+
+    barLabels.add(item.stateDt);
+
+    if (!covidAgeInfo[item.gubun]) {
+      covidAgeInfo[item.gubun] = {
+        [item.stateDt]: item.confCase,
+      };
+
+      return;
+    }
+
+    covidAgeInfo[item.gubun][item.stateDt] = item.confCase;
+  });
+
+  const barData = {
+    labels: [...barLabels]
+      .reverse()
+      .map((date) => moment(date).format("MM/DD")),
+    datasets: ageGubunList.map((label) => {
+      return {
+        label,
+        data: [...barLabels]
+          .reverse()
+          .map((item) => Number(covidAgeInfo[label][item])),
+        backgroundColor: colorInfo[label as keyof typeof colorInfo],
+      };
+    }),
+  };
+
+  return (
+    <ChartSection title="코로나 일자별 확진자 수">
+      <StackedBarChart options={options} data={barData} />
+    </ChartSection>
+  );
+}
+
+export default CovidAge;
