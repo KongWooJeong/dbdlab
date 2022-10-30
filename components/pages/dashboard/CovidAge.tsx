819d15db87e6acf7cb0c3577a500b667e4676df8
woojeong kong <coolstevevc@gmail.com>
2022-10-30 13:11:23 +0900

    feat(component): add CovidDaily component
    The number of confirmed cases of covid by date is shown using a line chart

---
+++ components/pages/dashboard/CovidAge.tsx
@@ -0,0 +1,50 @@
+import moment from "moment";
+
+import ChartSection from "../../chart/ChartSection";
+import LineChart from "../../chart/LineChart";
+
+interface CovidDayInfo {
+  decideCnt: string;
+  stateDt: string;
+  stateTime: string;
+}
+
+interface Props {
+  data: CovidDayInfo[];
+}
+
+const options = {
+  plugins: {
+    legend: {
+      display: false,
+    },
+    title: {
+      display: false,
+    },
+  },
+  maintainAspectRatio: false,
+};
+
+function CovidAge({ data }: Props) {
+  const lineLabels = data.map((item) => moment(item.stateDt).format("MM/DD"));
+
+  const lineChartdata = {
+    labels: lineLabels.reverse(),
+    datasets: [
+      {
+        label: "daily covid",
+        data: data.map((item) => Number(item.decideCnt)),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+    ],
+  };
+
+  return (
+    <ChartSection title="코로나 일자별 확진자 수">
+      <LineChart options={options} data={lineChartdata} />
+    </ChartSection>
+  );
+}
+
+export default CovidAge;
