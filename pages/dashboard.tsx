6afa6b0e21e15d0021ce7fd6dbdfb8c8a9b9e992
woojeong kong <coolstevevc@gmail.com>
2022-10-30 13:13:00 +0900

    feat(component): add get covid data logic
    Added logic to get covid using the getServerSideProps method and pass it as props

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,11 +1,26 @@
 import styled from "@emotion/styled";
+import axios from "axios";
 
-function Dashboard() {
+import CovidAge from "../components/pages/dashboard/CovidAge";
+
+interface CovidDayInfo {
+  decideCnt: string;
+  stateDt: string;
+  stateTime: string;
+}
+
+interface Props {
+  covidDayInfo: CovidDayInfo[];
+}
+
+function Dashboard({ covidDayInfo }: Props) {
   return (
     <DashboardWrapper>
-      <div className="first-row-container">첫번째</div>
+      <div className="first-row-container">
+        <CovidAge data={covidDayInfo} />
+      </div>
       <div className="second-row-container">
-        <div className="first-column-container">두번째</div>
+        <div className="first-column-container">두번쨰</div>
         <div className="second-column-container">세번째</div>
       </div>
     </DashboardWrapper>
@@ -19,23 +34,33 @@ const DashboardWrapper = styled.div`
   background-color: #fff;
 
   .first-row-container {
-    height: 50%;
+    height: 100%;
   }
 
   .second-row-container {
     display: flex;
-    height: 50%;
+    height: 100%;
 
     .first-column-container {
       width: 50%;
-      height: 100%;
     }
 
     .second-column-container {
       width: 50%;
-      height: 100%;
     }
   }
 `;
+
+export async function getServerSideProps() {
+  const { data } = await axios.get(
+    "http://localhost:3000/covidData/getCovid19InfState.json"
+  );
+
+  return {
+    props: {
+      covidDayInfo: data.items.item,
+    },
+  };
+}
 
 export default Dashboard;
